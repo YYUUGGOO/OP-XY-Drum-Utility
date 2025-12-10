2f9d816af95b20505079622cabda77c2d30588b6
Yuugo <cyberlinguist@yahoo.com>
2025-12-10 14:10:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  // Use repo name so assets load correctly on GitHub Pages
+  base: '/OP-XY-Drum-Utility/',
   plugins: [react()],
   server: {
     port: 5173
